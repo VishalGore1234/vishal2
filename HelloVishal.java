2c847483def425f38a7a56f0285f5d0d132d6afa
vishal gore <vishalngore45@gmail.com>
2024-12-29 23:45:39 -0800

    first commit

---
+++ HelloVishal.java
@@ -0,0 +1,8 @@
+public class HelloVishal {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("The sum of 5 and 10 is: " + sum);
+ }
+}
